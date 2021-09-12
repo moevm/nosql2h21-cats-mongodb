43d9b19e4a7e8db4bb3919778f6abf6815da4eca
k.kostarev <k.kostarev@tinkoff.ru>
2021-09-12 16:45:22 +0300

    Rename cats-app to cats-app-frontend

--- cats-app-front/karma.conf.js
+++ cats-app-front/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/cats-app'),
+      dir: require('path').join(__dirname, './coverage/cats-app-front'),
       subdir: '.',
       reporters: [
         { type: 'html' },
